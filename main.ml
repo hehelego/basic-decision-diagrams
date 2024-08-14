5967c0ae5ff4866bc264e3fb00e21efe1c5b457f
hehelego <hahalegao@gmail.com>
2024-08-14 20:40:45 +0800

    ZDD random testing

--- main.ml
+++ main.ml
@@ -83,9 +83,9 @@ let bdd_tests iters () =
     add (Bdd.op_nand builder p q) (TruthTable.op_nand p' q');
     add (Bdd.op_imply builder p q) (TruthTable.op_imply p' q');
     add (Bdd.op_equiv builder p q) (TruthTable.op_equiv p' q');
-    (* (* unary operator *) *)
+    (* unary operator *)
     add (Bdd.op_not builder p) (TruthTable.op_not p');
-    (* (* ternary if-then-else *) *)
+    (* ternary if-then-else *)
     add (Bdd.if_then_else builder r p q) (TruthTable.op_ite r' p' q');
     (* substitution *)
     add (Bdd.subs builder p x q) (TruthTable.subs p' x q');
@@ -98,14 +98,90 @@ let bdd_tests iters () =
     ()
   done
 
-let zdd_tests () =
+module SetFamily = struct
+  let lim = 9
+
+  module S = Set.Make (struct
+    type t = int
+
+    let compare = compare
+  end)
+
+  let to_bitmask = List.fold_left (fun acc x -> acc lor (1 lsl x)) 0
+  let from_list_list xs = List.map to_bitmask xs |> S.of_list
+  let empty = S.empty
+  let singleton x = S.singleton (1 lsl x)
+  let single_set xs = to_bitmask xs |> S.singleton
+  let insert x = S.map (( lor ) (1 lsl x))
+  let remove x xs = S.map (( land ) (lnot (1 lsl x))) xs
+  let filter0 x xs = S.filter (fun xs -> (xs lsr x) land 1 = 0) xs
+  let filter1 x xs = S.filter (fun xs -> (xs lsr x) land 1 = 1) xs |> remove x
+  let union = S.union
+  let inter = S.inter
+  let diff = S.diff
+  let sym_diff xs ys = union (diff xs ys) (diff ys xs)
+  let count = S.cardinal
+  let is_subset xs ys = S.for_all (fun x -> S.mem x ys) xs
+  let equal xs ys = is_subset xs ys && is_subset ys xs
+end
+
+let zdd_tests iters () =
+  Random.init 0;
+
   let builder = Zdd.builder () in
-  let _ = builder in
-  ()
+  let buf = Dynarray.create () in
+  Dynarray.ensure_capacity buf (iters * 15);
+  let add dd set =
+    let set' = Zdd.family builder dd |> SetFamily.from_list_list in
+    assert (SetFamily.equal set set');
+    Dynarray.add_last buf (dd, set)
+  in
+  let pick () = Dynarray.length buf |> Random.int |> Dynarray.get buf in
+  let draw_var () = Random.int TruthTable.lim in
+
+  print_endline "[ZDD] empty family";
+  add Zdd.empty SetFamily.empty;
+
+  print_endline "[ZDD] singleton families";
+  for i = 0 to SetFamily.lim - 1 do
+    add (Zdd.singleton builder i) (SetFamily.singleton i)
+  done;
+
+  print_endline "[ZDD] single set special case";
+  add (Zdd.single_set builder []) (SetFamily.single_set []);
+  add (Zdd.single_set builder [ 0 ]) (SetFamily.single_set [ 0 ]);
+  let all = List.init SetFamily.lim (fun x -> x) in
+  add (Zdd.single_set builder all) (SetFamily.single_set all);
+
+  print_endline "[ZDD] manipulations";
+  for _ = 1 to iters do
+    let p, p' = pick () in
+    let q, q' = pick () in
+    let x = draw_var () in
+    (* set operations *)
+    add (Zdd.union builder p q) (SetFamily.union p' q');
+    add (Zdd.inter builder p q) (SetFamily.inter p' q');
+    add (Zdd.diff builder p q) (SetFamily.diff p' q');
+    add (Zdd.sym_diff builder p q) (SetFamily.sym_diff p' q');
+    (* filtering *)
+    add (Zdd.filter0 builder x p) (SetFamily.filter0 x p');
+    add (Zdd.filter1 builder x q) (SetFamily.filter1 x q');
+    (* insert/remove *)
+    add (Zdd.insert builder x p) (SetFamily.insert x p');
+    add (Zdd.remove builder x q) (SetFamily.remove x q');
+    (* counting *)
+    assert (Zdd.count builder p = SetFamily.count p');
+    assert (Zdd.count builder q = SetFamily.count q');
+    (* from list *)
+    let len = Random.int (SetFamily.lim * 4) in
+    let xs = List.init len (fun _ -> Random.int SetFamily.lim) in
+    add (Zdd.single_set builder xs) (SetFamily.single_set xs);
+    ()
+  done
 
 let () =
   print_endline "Testing Binary Decision Diagrams          ...";
   bdd_tests 100000 ();
   print_endline "Testing Zero-Suppressed Decision Diagrams ...";
-  zdd_tests ();
+  zdd_tests 100000 ();
   ()
